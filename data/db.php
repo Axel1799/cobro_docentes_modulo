6eb28d30960f3b4523d8e39b27766df427743fdd
Axel1799 <48534774+Axel1799@users.noreply.github.com>
2022-04-11 19:27:48 -0400

    Update db.php

--- data/db.php
+++ data/db.php
@@ -1,7 +1,7 @@
 <?php
 
 $mysqli = new mysqli("localhost", "root", "", "cobros_docentes");
-
+$mysqli->set_charset("utf8");
 /* verificar la conexión */
 if (mysqli_connect_errno()) {
     printf("Conexión fallida: %s\n", mysqli_connect_error());
